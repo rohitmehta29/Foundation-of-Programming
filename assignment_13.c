a0815c5dd50f9468a0e8f8d0677c66f9cd50cc17
rohitmehta29 <rohitkailashmehta57@gmail.com>
2026-03-30 20:05:42 +0530

    Add files via upload

---
+++ assignment_13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int num,digit;
+    printf("Enter a number:");
+    scanf("%d",&num);
+
+    printf("Digit in reverse order:");
+    while(num>0)
+    {
+        digit = num%10;
+        printf("%d",digit);
+        num = num/10;
+
+    }
+    return 0;
+}
